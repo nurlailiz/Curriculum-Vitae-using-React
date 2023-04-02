ffdca50e008d475ead170bcffdb815c390c4b845
Nurlaili Zainal <97009898+nurlailiz@users.noreply.github.com>
2023-04-02 14:45:32 +0800

    Upload JS and CSS file

---
+++ App.js
@@ -0,0 +1,165 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>NURLAILI BINTI ZAINAL ABIDIN</h3>
+        <div className="Contact">
+          <div><p>&#40;+60&#41;124469907</p></div>
+          <div><p>nurlailiz@gmail.com</p></div>
+        </div>
+
+        <a
+          className="Link"
+          href="https://github.com/nurlailiz"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://github.com/nurlailiz
+        </a>
+        <a
+          className="Link"
+          href="https://www.linkedin.com/in/nurlaili-zainal-abidin-211501209/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://www.linkedin.com/in/nurlaili-zainal-abidin-211501209/
+        </a>
+
+        <p className="Desc">
+        Self-motivated and open to new experiences and learning new skills. 
+        Freshly graduated from a South Korean university and currently open to work starting April 2023. 
+        Possessed multitasking and decision making skills from interest and experiences in playing different types of video games. Holds sufficient problem-solving and creative thinking skills.
+        </p>
+      </header>
+
+      <p className="Section-title">EDUCATION</p>
+      <div className="Border"></div>
+      <p className="Bold-item">Sungkyunkwan University, South Korea</p>
+      <div className="Education-year">
+        <div><p>Bachelor's Degree in Software</p></div>
+        <div><p>2023</p></div>
+      </div>
+      <p className="Content">CGPA: 3.32</p>
+
+      <p className="Bold-item">University Kuala Lumpur Malaysian Institute of Information Technology, Malaysia</p>
+      <div className="Education-year">
+        <div><p>Foundation in Science and Technology for Korean Universities</p></div>
+        <div><p>2018</p></div>
+      </div>
+      <p className="Content">CGPA: 3.84</p>
+
+      <p className="Bold-item">MARA Junior Science College &#40;MRSM&#41; Tun Ghafar Baba, Malaysia</p>
+      <div className="Education-year">
+        <div><p>SPM Certificate &#40;O-Level&#41;</p></div>
+        <div><p>2016</p></div>
+      </div>
+      <div className="Education-cert">
+        <div><p>CGPA: 3.87</p></div>
+        <div><p>SPM Certificate: 3A+ 6A</p></div>
+        <div><p>1119 GCE-O Grade: 1A</p></div>
+      </div>
+
+      <p className="Section-title">EXPERIENCE</p>
+      <div className="Border"></div>
+      <p className="Bold-item">Content Creator Freelancer, Skincarehouse.co</p>
+      <p className="Content">May 2022 - August 2022 | Malaysia</p>
+      <ul>
+        <li>Proposed daily content ideas for company's Instagram feed.</li>
+        <li>Designed contents posts for company's Instagram account.</li>
+      </ul>
+
+      <p className="Bold-item">Media Team, Malaysian Students Dance Crew in South Korea &#40;MSDC&#41;</p>
+      <p className="Content">April 2021 - August 2022 | South Korea</p>
+      <ul>
+        <li>Designed posters and infographics for MSDC official Instagram account.</li>
+        <li>Recorded and edited performance videos by members of MSDC and post on MSDC's official YouTube account.</li>
+      </ul>
+
+      <p className="Bold-item">Software Quality Assurance Intern, Pandai Education Sdn Bhd</p>
+      <p className="Content">January 2022 - February 2022 | Malaysia</p>
+      <ul>
+        <li>Assisted in Quality Assurance testing for the Pandai mobile application for both iOS and Android every time a new version of the app is to be released to production.</li>
+        <li>Worked together with the development team in ensuring the app runs smoothly without any bugs and errors.</li>
+      </ul>
+
+      <p className="Section-title">VOLUNTEERING EXPERIENCE</p>
+      <div className="Border"></div>
+      <p className="Bold-item">Fundraiser, Hand in Hand: FUN-ME 2021</p>
+      <p className="Content">June 2021 | Malaysia</p>
+      <ul>
+        <li>Participated as a fundraiser for a fundraising campaign aimed towards Education Inequality in Malaysia and successfully raised MYR 905 to the accumulation of fundraising campaign worth MYR 12 400.</li>
+      </ul>
+
+      <p className="Bold-item">Volunteer, Burger For All 2017</p>
+      <p className="Content">January 2017 | Malaysia</p>
+      <ul>
+        <li>Participated in distributing care packages containing daily necessities and burgers to the homeless people in 4 areas in Kuala Lumpur which are Masjid Jamek, Pudu, Pasar Seni and Kolej SeGi.</li>
+      </ul>
+
+      <p className="Section-title">SELECTED AWARDS AND HONORS</p>
+      <div className="Border"></div>
+      <p className="Bold-item">3rd Place in Show Us Your Korea YouTube Video Contest for Foreign Nationals in Korea</p>
+      <p className="Content">Organized by Funday Korea Networks and Allmytour, 2020 | South Korea</p>
+
+      <p className="Bold-item">Awards for Achieving Excellent Results in Sijil Pelajaran Malaysia &#40;SPM&#41; 2016</p>
+      <p className="Content">Awarded by the Malacca State Government and DRB-HICOM Berhad, 2017 | Malaysia</p>
+
+      <p className="Section-title">PROJECT</p>
+      <div className="Border"></div>
+      <p className="Bold-item">Final Year Graduation Project</p>
+      <p className="Content">January 2022 - December 2022 | Sungkyunkwan University, South Korea</p>
+      <ul>
+        <li>Worked in a team on a project titled <span style={{ fontWeight:'bold'}}>'Smartphone-based Dietary Assessment using Semantic Ontology and Deep Learning'</span> which is a mobile application that assist users in checking whether commercialized food products comply with their dietary preferences.</li>
+        <li>In charge of the front-end development of the project and connection between the front-end with the server side.</li>
+        <li>Utilized Flutter in Android Studio to create the user interface of the mobile application in addition to making use of other packages such as Optical Character Recognition In Flutter.</li>
+      </ul>
+
+      <p className="Bold-item">Open Source Software Lab Class Final Project</p>
+      <p className="Content">October 2022 - November 2022 | Sungkyunkwan University, South Korea</p>
+      <ul>
+        <li>Worked alongside a teammate in creating a web application user interface called 'FoodOverflow' which is a question and answer website for enthusiast food lovers inspired by Stack Overflow. The project is published on GitHub.</li>
+        <li>Utilized programming languages such as HTML, CSS and JavaScript.</li>
+      </ul>
+
+      <p className="Section-title">SKILLS</p>
+      <div className="Border"></div>
+      <p className="Bold-item">Traditional & Digital Drawing</p>
+      <p className="Content">Krita&ensp; | &ensp;MediBang Paint&ensp; | &ensp;Procreate</p>
+
+      <p className="Bold-item">Graphic Design</p>
+      <p className="Content">Canva</p>
+
+      <p className="Bold-item">Video Editing</p>
+      <p className="Content">Wondershare Filmora 11&ensp; | &ensp;CapCut&ensp; | &ensp;Corel Videostudio 2019&ensp; | &ensp;iMovie</p>
+
+      <p className="Bold-item">Programming Languages &#40;Intermediate&#41;</p>
+      <p className="Content">HTML&ensp; | &ensp;CSS&ensp; | &ensp;JavaScript&ensp; | &ensp;Java&ensp; | &ensp;Dart&ensp; | &ensp;C&ensp; | &ensp;C++</p>
+
+      <p className="Bold-item">Programming Software</p>
+      <p className="Content">Android Studio&ensp; | &ensp;Atom&ensp; | &ensp;Visual Studio Code</p>
+
+      <p className="Section-title">LANGUAGES</p>
+      <div className="Border"></div>
+      <p className="Bold-item">Malay <span style={{ fontWeight:'normal'}}>| Native</span></p>
+      
+      <p className="Bold-item">English <span style={{ fontWeight:'normal'}}>| Proficient</span></p>
+      <p className="Content">Certificate: International English Language Testing System Band 7.5, CEFR Level: C1 &#40;2018&#41;</p>
+
+      <p className="Bold-item">Korean <span style={{ fontWeight:'normal'}}>| Intermediate</span></p>
+      <p className="Content">Certificate: Test of Proficiency in Korean Level 4 &#40;2017-2020&#41;</p>
+
+      <p className="Section-title">REFERENCE</p>
+      <div className="Border"></div>
+      <p className="Bold-item">Tamer Abuhmed</p>
+      <p className="Content-ref">Associate Professor at Sungkyunkwan University | South Korea</p>
+      <p className="Content-ref">Email: tamer@skku.edu</p>
+      <p className="Content-ref">Phone: +82-31-290-7968</p>
+
+
+    </div>
+  );
+}
+
+export default App;
